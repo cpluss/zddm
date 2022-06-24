9a110e59b54cff8d81893b7aa6a6e3852f9644c2
Sebastian Lund <sebastian.lund@superbet.com>
2022-06-24 14:19:20 +0100

    [cpp] add main proxy behaviour
    Simple set-up right now. No error or fault management whatsoever and a simple traffic split with dual writes.

---
+++ lib/cpp/src/zddm/Proxy-Inl.h
@@ -0,0 +1,72 @@
+#ifndef ZDDM_PROXY_H_
+#error "This file may only be included by zddm.h"
+#endif
+
+namespace zddm {
+
+namespace detail {
+
+template <typename T>
+T copy(T t) {
+  return t;
+}
+
+}  // namespace detail
+
+template <typename K, typename T>
+T Proxy<K, T>::read(K const& key) const {
+  // We assume that if the proxy is not enabled
+  // we can query the old storage solution, and safely
+  // unwrap the return value as it is expected to always
+  // work (or throw exceptions otherwise).
+  if (!isEnabled()) {
+    return std::move(old_->read(key).value());
+  }
+
+  // First we try to hit `new_`
+  auto maybeData = new_->read(key);
+  // On a hit return the value, as `new_` should be the
+  // source of truth for anything we find therein.
+  if (maybeData.has_value()) {
+    return std::move(maybeData.value());
+  }
+
+  // On a miss we return the value from `old_`, assuming
+  // it would be found or return a valid value.
+  maybeData = old_->read(key);
+  if (!maybeData) {
+    throw new std::out_of_range(
+        "[ZDDM][Proxy] could not find key in old storage");
+  }
+
+  // Write data to the new storage & then return
+  auto data = std::move(maybeData.value());
+
+  // Note that this is currently blocking, which we may want
+  // to change in the future as it can have quite a drastic
+  // latency impact on large workloads.
+  new_->write(key, detail::copy(data));
+  return std::move(data);
+}
+
+template <typename K, typename T>
+void Proxy<K, T>::write(K const& key, T&& data) {
+  // We assume that we would always, even if disabled, want to
+  // write to the old storage. It's the de-facto source
+  // of truth.
+  //
+  // Note that this is split up in order to avoid an unnecessary
+  // data copy (which the language does implicitly).
+  if (!isEnabled()) {
+    old_->write(key, std::move(data));
+    return;
+  }
+
+  // Perform a double-write. If either write fail we'd rather have
+  // the old storage failing first and avoid writing at all
+  // to the new storage.
+  old_->write(key, detail::copy(data));
+  new_->write(key, std::move(data));
+}
+
+}  // namespace zddm
---
+++ lib/cpp/src/zddm/Proxy.h
@@ -0,0 +1,74 @@
+#pragma once
+#define ZDDM_PROXY_H_
+
+#include <optional>
+
+#include "zddm/StorageAdapter.h"
+
+namespace zddm {
+
+template <typename K, typename T>
+class Proxy {
+ public:
+  Proxy(std::unique_ptr<StorageAdapter<K, T>> &&old_storage,
+        std::unique_ptr<StorageAdapter<K, T>> &&new_storage)
+      : old_(std::move(old_storage)),
+        new_(std::move(new_storage)),
+        enabled_(true) {}
+
+  /**
+   * @brief Perform a proxied read from either of the storage
+   * adapters, depending on where the data can be found (
+   * if at all).
+   *
+   * @param key The key over which to read the data, this
+   * would often map to your primary key, or some index.
+   */
+  T read(K const &key) const;
+
+  /**
+   * @brief Perform a proxied write to both of the storage
+   * adapters, depending on where the data can be found.
+   *
+   * Note that this only perform writes to the new database if
+   * and only if the key is caught by the traffic-gate as configured.
+   *
+   * @param key The key over which to read the data, this
+   * would often map to your primary key, or some index.
+   * @param data The actual data to write into each storage
+   * adapter.
+   */
+  void write(K const &key, T &&data);
+
+  /**
+   * @brief Enable the proxy functionality, which will
+   * enable traffic being sent to the new storage backend.
+   */
+  Proxy &enable() {
+    enabled_ = true;
+    return *this;
+  }
+
+  /**
+   * @brief Disable the proxy functionality, which will
+   * disable traffic being sent to the new storage backend.
+   */
+  Proxy &disable() {
+    enabled_ = false;
+    return *this;
+  }
+
+  /**
+   * @brief Indicator whether or not the proxy is enabled
+   */
+  bool isEnabled() const { return enabled_; }
+
+ private:
+  std::unique_ptr<StorageAdapter<K, T>> old_;
+  std::unique_ptr<StorageAdapter<K, T>> new_;
+  bool enabled_;
+};
+
+}  // namespace zddm
+
+#include "Proxy-Inl.h"
